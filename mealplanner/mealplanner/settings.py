9bb844cd597c74941b567c6039b9fabfbdea3132
Giovanni Bruner <giovannibruner@Giovannis-MacBook-Air.local>
2020-06-23 23:16:00 +0200

    fixing secret key

--- mealplanner/mealplanner/settings.py
+++ mealplanner/mealplanner/settings.py
@@ -19,6 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
+import os
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
